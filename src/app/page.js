49a0baa1e31ff618dd3cde8a9229ce1c12ebc13a
mosalah123 <408737768moe.dog@gmail.com>
2025-04-04 23:47:20 +0200

    fix home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 import HomePage from "./Home/page";
 import NavBar from "./Shared/navBar/page";
-// import Register from "./user/register/page";
-import PaitentDash from "./PatiantDashboard/page";
 
 export default function Home() {
   return (
     <div className="">
       <NavBar />
-      {/* <HomePage /> */}
-      <PaitentDash />
+      <HomePage />
+      {/* <PaitentDash /> */}
     </div>
   );
 }
